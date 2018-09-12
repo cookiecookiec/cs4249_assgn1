422e5977402a876fba634ddb6540e7a03b5a9baf
cookiecookiec <jessy_cookie@hotmail.com>
2018-09-12 18:41:27 +0800

    Update experiment.js

--- js/experiment.js
+++ js/experiment.js
@@ -24,7 +24,7 @@ var markingMenuSubscription = null;
 var radialMenuSvg = null;
 
 
-unction GetQueryString(name) {
+function GetQueryString(name) {
 	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
 	var r = window.location.search.substr(1).match(reg);
 	if(r!=null)return  unescape(r[2]); return null;
